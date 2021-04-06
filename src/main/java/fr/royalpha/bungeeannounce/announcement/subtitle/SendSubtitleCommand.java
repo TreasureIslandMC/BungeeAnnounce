4a5ff61e57fec109d824b6c94391ac7cd4f29c13
Omer Oreg <sarhatabaot1@gmail.com>
2021-04-06 20:48:47 +0300

    Update SendSubtitleCommand.java

--- src/main/java/fr/royalpha/bungeeannounce/announcement/subtitle/SendSubtitleCommand.java
+++ src/main/java/fr/royalpha/bungeeannounce/announcement/subtitle/SendSubtitleCommand.java
@@ -21,7 +21,9 @@ public class SendSubtitleCommand extends Command {
 			sender.sendMessage(new TextComponent(ChatColor.RED + "Usage: /sendsubtitle <fadeIn> <stay> <fadeOut> <title>"));
 			return;
 		}
-		Integer fadeIn, stay, fadeOut;
+		int fadeIn;
+		int stay;
+		int fadeOut;
 		try {
 		    fadeIn = Integer.parseInt(args[0])*20;
 		    stay = Integer.parseInt(args[1])*20;
