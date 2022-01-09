2f3ecf682849e6a919862fb310b56a51ed965fd1
Omer Oreg <sarhatabaot1@gmail.com>
2022-01-09 16:05:29 +0200

    Removed Channels

--- src/main/java/fr/royalpha/bungeeannounce/BungeeAnnouncePlugin.java
+++ src/main/java/fr/royalpha/bungeeannounce/BungeeAnnouncePlugin.java
@@ -1,26 +1,21 @@
 package fr.royalpha.bungeeannounce;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import co.aikar.commands.BungeeCommandManager;
 import fr.royalpha.bungeeannounce.command.BungeeAnnounceCommand;
+import fr.royalpha.bungeeannounce.handler.Logger;
+import fr.royalpha.bungeeannounce.manager.AnnouncementManager;
+import fr.royalpha.bungeeannounce.manager.ConfigManager;
 import fr.royalpha.bungeeannounce.manager.MsgManager;
 import fr.royalpha.bungeeannounce.task.ScheduledAnnouncement;
-import fr.royalpha.bungeeannounce.handler.Logger;
-import fr.royalpha.bungeeannounce.handler.PlayerAnnouncer;
-import fr.royalpha.bungeeannounce.handler.PlayerAnnouncer.ConnectionType;
-import fr.royalpha.bungeeannounce.manager.AnnouncementManager;
-import fr.royalpha.bungeeannounce.manager.ChannelManager;
-import fr.royalpha.bungeeannounce.manager.ConfigManager;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginManager;
-import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.config.Configuration;
 import org.bstats.bungeecord.Metrics;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Royalpha
@@ -28,7 +23,7 @@ import org.bstats.bungeecord.Metrics;
 public class BungeeAnnouncePlugin extends Plugin implements Listener {
 	
 	private static BungeeAnnouncePlugin instance;
-	private static Logger logSystem;
+	private Logger logSystem;
 
 	private ConfigManager configManager;
 	private List<ScheduledAnnouncement> scheduledAnnouncement;
@@ -45,7 +40,8 @@ public class BungeeAnnouncePlugin extends Plugin implements Listener {
 	@Override
 	public void onEnable() {
 		instance = this;
-		msgManager = new MsgManager();
+		this.msgManager = new MsgManager();
+		this.msgManager.loadIgnoredPlayers(this);
 		loadConfigFile();
 		initializeLogSystem();
 		loadConfigContent();
@@ -56,7 +52,12 @@ public class BungeeAnnouncePlugin extends Plugin implements Listener {
 		initializeMetrics();
 	}
 
-	
+	@Override
+	public void onDisable() {
+		final Configuration ignored = configManager.getIgnored();
+		ignored.set("ignored",msgManager.getPlayerIgnoreCache());
+	}
+
 	/*
 	 * To do better, the load method should be executed in the onEnable to avoid repeating lines of code. However, since it is only used for the BAReload command, I decided to separate it from the onEnable.
 	 */
@@ -65,7 +66,6 @@ public class BungeeAnnouncePlugin extends Plugin implements Listener {
 		logSystem = new Logger(this);
 		
 		this.scheduledAnnouncement = this.configManager.loadScheduledAnnouncement();
-		this.configManager.loadAutoPlayerAnnouncement();
 	}
 	private void loadConfigFile() {
 		this.configManager = new ConfigManager(this);
@@ -75,8 +75,6 @@ public class BungeeAnnouncePlugin extends Plugin implements Listener {
 	}
 	private void loadConfigContent() {
 		this.scheduledAnnouncement = this.configManager.loadScheduledAnnouncement();
-		this.configManager.loadAutoPlayerAnnouncement();
-		this.configManager.loadChannels();
 	}
 
 	private void registerCommands(){
@@ -95,46 +93,6 @@ public class BungeeAnnouncePlugin extends Plugin implements Listener {
 		Metrics metrics = new Metrics(this, 8662);
 	}
 
-	@EventHandler
-	public void onConnect(final net.md_5.bungee.api.event.ServerConnectedEvent event) {
-		final ProxiedPlayer player = event.getPlayer();
-		List<PlayerAnnouncer> autoPlayerAnnouncements = PlayerAnnouncer.getAnnouncementList(player, event.getServer(), ConnectionType.CONNECT_SERVER);
-		if (!autoPlayerAnnouncements.isEmpty()) {
-			for (PlayerAnnouncer playerAnnouncer : autoPlayerAnnouncements)
-				getProxy().getScheduler().schedule(this, () -> AnnouncementManager.sendToServer(playerAnnouncer.getAnnouncement(), getProxy().getConsole(), player, playerAnnouncer.getMessage(), playerAnnouncer.getBroadcastServers(), false, "", playerAnnouncer.getOptionalTitleArgs()), 500, TimeUnit.MILLISECONDS);
-		}
-
-		//autojoin
-		for(ChannelManager channel: ChannelManager.getChannels()) {
-			if(channel.isAutoJoin() && player.hasPermission(channel.getPermission())) {
-				channel.joinPlayer(player);
-			}
-		}
-	}
-	
-	@EventHandler
-	public void onChat(final net.md_5.bungee.api.event.ChatEvent event) {
-		if (!event.isCommand() && event.getSender().isConnected() && event.getSender() instanceof ProxiedPlayer) {
-			final ProxiedPlayer player = (ProxiedPlayer) event.getSender();
-			final List<ChannelManager> channels = ChannelManager.getPlayerChannels(player);
-			if (channels.size() == 1) {
-				ChannelManager channel = channels.get(0);
-				channel.sendMessage(player, event.getMessage());
-				event.setCancelled(true);
-			}
-		}
-	}
-	
-	@EventHandler
-	public void onDisconnect(final net.md_5.bungee.api.event.PlayerDisconnectEvent event) {
-		final ProxiedPlayer player = event.getPlayer();
-		List<PlayerAnnouncer> autoPlayerAnnouncements = PlayerAnnouncer.getAnnouncementList(player, event.getPlayer().getServer(), ConnectionType.LEAVE_PROXY);
-		if (!autoPlayerAnnouncements.isEmpty()) {
-			for (PlayerAnnouncer playerAnnouncer : autoPlayerAnnouncements)
-				getProxy().getScheduler().schedule(this, () -> AnnouncementManager.sendToServer(playerAnnouncer.getAnnouncement(), getProxy().getConsole(), player, playerAnnouncer.getMessage(), playerAnnouncer.getBroadcastServers(), false, "", playerAnnouncer.getOptionalTitleArgs()), 500, TimeUnit.MILLISECONDS);
-		}
-	}
-	
 	public List<ScheduledAnnouncement> getScheduledAnnouncement() {
 		return this.scheduledAnnouncement;
 	}
@@ -147,7 +105,12 @@ public class BungeeAnnouncePlugin extends Plugin implements Listener {
 		return instance.getProxy();
 	}
 	
-	public static Logger getLoggerSystem() {
+	public Logger getLoggerSystem() {
 		return logSystem;
+	}
+
+
+	public static BungeeAnnouncePlugin instance() {
+		return instance;
 	}
 }
